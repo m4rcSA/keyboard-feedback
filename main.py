4c9374e42c8124cfc375c80b84b58912994196fb
m4rcSA <43429867+m4rcSA@users.noreply.github.com>
2023-06-15 09:27:48 +0200

    Disable repeat on hold

--- main.py
+++ main.py
@@ -11,22 +11,27 @@ import click
 enter = None
 
 keys = []
+pressed_keys = set()
 
 def init(device="mechanical"):
     global enter, keys
     pygame.mixer.init()
     enter = pygame.mixer.Sound(f"assets/{device}/enter_click.wav")
-    for i in range(1,7):
+    for i in range(1, 7):
         keys.append(pygame.mixer.Sound(f"assets/{device}/key_{i}.wav"))
 
 def play_key(key):
-
-    if key==Key.enter:
-        enter.play()
-    elif key:
+    if key == Key.enter:
+        if Key.enter not in pressed_keys:
+            pressed_keys.add(Key.enter)
+            enter.play()
+    elif key and key not in pressed_keys:
+        pressed_keys.add(key)
         random.choice(keys).play()
-    else:
-        enter.play()
+
+def release_key(key):
+    if key in pressed_keys:
+        pressed_keys.remove(key)
 
 @click.command()
 @click.option('-t', '--typewriter', is_flag=True)
@@ -38,9 +43,8 @@ def main(typewriter):
         init()
 
     # Create a listener for the keyboard
-    listener = keyboard.Listener(on_press=play_key)
-    listener.start()  # start the listener
-    listener.join()  # join the listener thread to the main thread to keep waiting for keys
+    with keyboard.Listener(on_press=play_key, on_release=release_key) as listener:
+        listener.join()  # join the listener thread to the main thread to keep waiting for keys
 
 
 if __name__ == '__main__':
