2eb42bacf612e9f998e65a10eea10fa30cef06db
m4rcSA <43429867+m4rcSA@users.noreply.github.com>
2023-06-17 05:41:09 +0200

    remove random sounds. keys defined on config.json

--- main.py
+++ main.py
@@ -1,50 +1,50 @@
 import sys
 import pygame
 import keyboard
-import random
-
+import json
 import click
 
-
 # Load the sound
-enter = None
-
-keys = []
+key_sounds = {}
 pressed_keys = set()
 
-def init(device="mechanical"):
-    global enter, keys
+def init(device_sound="nk-cream"):
+    global key_sounds
+
+    config_file = f"assets/{device_sound}/config.json"
+    with open(config_file) as f:
+        config = json.load(f)
+
     pygame.mixer.init()
-    enter = pygame.mixer.Sound(f"assets/{device}/enter_click.wav")
-    for i in range(1, 7):
-        keys.append(pygame.mixer.Sound(f"assets/{device}/key_{i}.wav"))
+    for key, value in config['defines'].items():
+        if value is not None:
+            key_sounds[int(key)] = pygame.mixer.Sound(f"assets/{device_sound}/{value}")
 
 def play_key(event, repeat_allowed=True):
-    if event.name == "enter":
-        if repeat_allowed or event.name not in pressed_keys:
-            pressed_keys.add(event.name)
-            enter.play()
-    elif event.name and (repeat_allowed or event.name not in pressed_keys):
-        pressed_keys.add(event.name)
-        random.choice(keys).play()
+    if event.scan_code in key_sounds and (repeat_allowed or event.scan_code not in pressed_keys):
+        pressed_keys.add(event.scan_code)
+        key_sounds[event.scan_code].play()
 
 def release_key(event):
-    if event.name in pressed_keys:
-        pressed_keys.remove(event.name)
+    if event.scan_code in pressed_keys:
+        pressed_keys.remove(event.scan_code)
 
 @click.command()
-@click.option('-s', '--sound', type=str, default='mechanical', help='Specify the soundpack (e.g., mechanical, typewriter)')
+@click.option('-s', '--device-sound', type=str, default='nk-cream', help='Specify the device sound folder name')
 @click.option('-r', '--repeat', is_flag=True, help='Enable key repeat')
-def main(sound, repeat):
+def main(device_sound, repeat):
     click.echo("Press CTRL + C to exit")
-    init(sound)
+    init(device_sound)
 
     # Create keyboard hooks
     keyboard.on_press(lambda event: play_key(event, repeat), suppress=True)
     keyboard.on_release(lambda event: release_key(event), suppress=True)
 
-    # Keep the script running until CTRL + c is pressed
-    keyboard.wait()
+    # Keep the script running indefinitely
+    try:
+        keyboard.wait()
+    except KeyboardInterrupt:
+        pass
 
 if __name__ == '__main__':
     main()
