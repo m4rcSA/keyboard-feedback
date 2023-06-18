ebdef0eb9c340df15921a4c2d95f736986128fa1
m4rcSA <43429867+m4rcSA@users.noreply.github.com>
2023-06-18 12:35:54 +0200

    add nk cream soundpack, random sound when key not defined

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 from pynput import keyboard
 import json
 import click
+import random
 
 # Load the sound
 key_sounds = {}
@@ -20,16 +21,38 @@ def init(device_sound="nk-cream"):
         if value is not None:
             key_sounds[int(key)] = pygame.mixer.Sound(f"assets/{device_sound}/{value}")
 
-def play_key(key, repeat_allowed=True, print_scan_code=False):
-    scan_code = getattr(key, 'scan_code', None)
-    if scan_code is not None and scan_code in key_sounds and (repeat_allowed or scan_code not in pressed_keys):
+def play_key(key, repeat_allowed=False, print_scan_code=False, debug=False):
+    try:
+        scan_code = key.vk
+        if debug:
+            click.echo(f"key.vk {scan_code}")
+    except AttributeError:
+        scan_code = key.value.vk
+        if debug:
+            click.echo(f"key.value.vk {scan_code}")
+
+    if scan_code in key_sounds and (repeat_allowed or scan_code not in pressed_keys):
+        if debug:
+            click.echo("key found")
         pressed_keys.add(scan_code)
         key_sounds[scan_code].play()
-    if print_scan_code:
-        print(f"Scan code: {scan_code}")
+    elif repeat_allowed or scan_code not in pressed_keys:
+        pressed_keys.add(scan_code)
+        random.choice(list(key_sounds.values())).play()
 
-def release_key(key):
-    scan_code = getattr(key, 'scan_code', None)
+    if print_scan_code:
+        click.echo(f"Scan code: {scan_code}")
+
+def release_key(key,debug=False):
+    try:
+        scan_code = key.vk
+        if debug:
+            click.echo(f"key.vk {scan_code}")
+    except AttributeError:
+        scan_code = key.value.vk
+        if debug:
+            click.echo(f"key.value.vk {scan_code}")
+    
     if scan_code is not None and scan_code in pressed_keys:
         pressed_keys.remove(scan_code)
 
@@ -37,14 +60,15 @@ def release_key(key):
 @click.option('-s', '--device-sound', type=str, default='nk-cream', help='Specify the device sound folder name')
 @click.option('-r', '--repeat', is_flag=True, help='Enable key repeat')
 @click.option('-p', '--print-scan-code', is_flag=True, help='Print the scan code of the pressed key')
-def main(device_sound, repeat, print_scan_code):
+@click.option('-d', '--debug', is_flag=True, help='Enable debug output')
+def main(device_sound, repeat, print_scan_code, debug):
     click.echo("Press CTRL + C to exit")
     init(device_sound)
 
     # Create keyboard listener
     listener = keyboard.Listener(
-        on_press=lambda key: play_key(key, repeat, print_scan_code),
-        on_release=lambda key: release_key(key)
+        on_press=lambda key: play_key(key, repeat, print_scan_code, debug),
+        on_release=lambda key: release_key(key, debug)
     )
     listener.start()
 
